e77ee8fccf3d876ebb472c3fb0b561f02f8d90ac
bradwyatt <bradjr@gmail.com>
2015-12-29 22:57:42 -0500

    Fixed Directory Problem

--- FishFood.py
+++ FishFood.py
@@ -22,6 +22,27 @@ lastPressed = 0
 allsprites = pygame.sprite.Group()
 clock = pygame.time.Clock()
 
+def adjust_to_correct_appdir():
+    try:
+        appdir = sys.argv[0] #feel free to use __file__
+        if not appdir:
+            raise ValueError
+        appdir = os.path.abspath(os.path.dirname(sys.argv[0]))
+        os.chdir(appdir)
+        if not appdir in sys.path:
+            sys.path.insert(0,appdir)
+    except:
+        #placeholder for feedback, adjust to your app.
+        #remember to use only python and python standard libraries
+        #not any resource or module into the appdir 
+        #a window in Tkinter can be adequate for apps without console
+        #a simple print with a timeout can be enough for console apps
+        print 'Please run from an OS console.'
+        import time
+        time.sleep(10)
+        sys.exit(1)
+adjust_to_correct_appdir()
+
 def load_sound(file, name):
     sound = pygame.mixer.Sound(file)
     sounds[name] = sound
