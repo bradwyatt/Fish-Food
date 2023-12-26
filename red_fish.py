0af54f947ae1ab3199f095cfcbfa6f251da1ec97
Brad Wyatt <bradjr@gmail.com>
2023-12-25 20:46:33 -0500

    red fish refactored

--- red_fish.py
+++ red_fish.py
@@ -3,49 +3,74 @@ import random
 from utils import SCREEN_WIDTH, SCREEN_HEIGHT  # Assuming you have a config.py with constants
 
 
+import pygame
+import random
+from utils import SCREEN_WIDTH, SCREEN_HEIGHT
+
 class RedFish(pygame.sprite.Sprite):
+    EDGE_PADDING = 100
+    CHANGE_DIR_RANGE = (100, 600)
+    SPEED_CHOICES = [-2, 0, 2]
+    WALL_PADDING = 32
+    BOTTOM_WALL_Y = SCREEN_HEIGHT - 64
+    RIGHT_WALL_X = SCREEN_WIDTH - 32
+
     def __init__(self, allsprites, images):
-        """
-        Weakest prey in the game
-        """
-        pygame.sprite.Sprite.__init__(self)
+        super().__init__()
         self.images = images
         self.image = images["spr_red_fish"]
         self.rect = self.image.get_rect()
         allsprites.add(self)
-        self.direction = (random.choice([-2, 2]), random.choice([-2, 0, 2]))
+        self.direction = (random.choice(self.SPEED_CHOICES), random.choice(self.SPEED_CHOICES))
         self.change_dir_timer = 0
-        self.rect.topleft = (random.randrange(100, SCREEN_WIDTH-100), random.randrange(100, SCREEN_HEIGHT-100))
+        self.reset_position()
+
     def update(self):
-        newpos = (self.rect.topleft[0] + self.direction[0],
-                  self.rect.topleft[1] + self.direction[1])
-        self.rect.topleft = newpos
+        self.rect.move_ip(*self.direction)
         self.change_dir_timer += 1
+        self.update_image_direction()
+        self.change_direction_randomly()
+
+    def update_image_direction(self):
         if self.direction[0] == -2:
-            self.image = pygame.transform.flip(self.images["spr_red_fish"], 1, 0)
+            self.image = pygame.transform.flip(self.images["spr_red_fish"], True, False)
         elif self.direction[0] == 2:
             self.image = self.images["spr_red_fish"]
-        if self.change_dir_timer > random.randrange(100, 600):
-            self.direction = random.choice([(self.direction[0]*-1, self.direction[1]),
-                                            (self.direction[0], self.direction[1]*-1),
-                                            (self.direction[0]*-1, self.direction[1]*-1)])
+
+    def change_direction_randomly(self):
+        if self.change_dir_timer > random.randrange(*self.CHANGE_DIR_RANGE):
+            self.direction = random.choice([(self.direction[0] * -1, self.direction[1]),
+                                            (self.direction[0], self.direction[1] * -1),
+                                            (self.direction[0] * -1, self.direction[1] * -1)])
             self.change_dir_timer = 0
+
     def collision_with_wall(self, rect):
         if self.rect.colliderect(rect):
             self.change_dir_timer = 0
-            if self.rect.left < 32: # Left walls
-                self.direction = (2, random.choice([-2, 0, 2]))
-            elif self.rect.top > SCREEN_HEIGHT-64: # Bottom walls
-                self.direction = (random.choice([-2, 0, 2]), -2)
-            elif self.rect.right > SCREEN_WIDTH-32: # Right walls
-                self.direction = (-2, random.choice([-2, 0, 2]))
-            elif self.rect.top < 32: # Top walls
-                self.direction = (random.choice([-2, 0, 2]), 2)
+            self.bounce_off_walls()
+
+    def bounce_off_walls(self):
+        if self.rect.left < self.WALL_PADDING:  # Left wall
+            self.direction = (2, random.choice(self.SPEED_CHOICES))
+        elif self.rect.top > self.BOTTOM_WALL_Y:  # Bottom wall
+            self.direction = (random.choice(self.SPEED_CHOICES), -2)
+        elif self.rect.right > self.RIGHT_WALL_X:  # Right wall
+            self.direction = (-2, random.choice(self.SPEED_CHOICES))
+        elif self.rect.top < self.WALL_PADDING:  # Top wall
+            self.direction = (random.choice(self.SPEED_CHOICES), 2)
+
+    def reset_position(self):
+        self.rect.topleft = (random.randrange(self.EDGE_PADDING, SCREEN_WIDTH - self.EDGE_PADDING),
+                             random.randrange(self.EDGE_PADDING, SCREEN_HEIGHT - self.EDGE_PADDING))
+
     def collide_with_player(self):
-        self.rect.topleft = (random.randrange(100, SCREEN_WIDTH-100), random.randrange(100, SCREEN_HEIGHT-100))
+        self.reset_position()
+
     def collide_with_bright_blue_fish(self):
-        self.rect.topleft = (random.randrange(100, SCREEN_WIDTH-100), random.randrange(100, SCREEN_HEIGHT-100))
+        self.reset_position()
+
     def collide_with_green_fish(self):
-        self.rect.topleft = (random.randrange(100, SCREEN_WIDTH-100), random.randrange(100, SCREEN_HEIGHT-100))
+        self.reset_position()
+
     def remove_sprite(self):
         self.kill()
